d2c1145c6df3f07e53a1aa7b8146b4016d442532
John <ema8720@gmail.com>
2019-01-04 23:11:41 -0800

    console log only working

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // immediate invoke function expression
-(function(){
+(function () {
 // create counter value
 let counterValue = 0;
 // get button with 
@@ -7,5 +7,33 @@ const buttons = document.querySelectorAll('.counterBtn');
 const counter = document.getElementById('counter');
 
 
+buttons.forEach(function(btn) {
+btn.addEventListener('click', function(event){
+// e.preventDefault();
+// set up an element that we click
+const value = event.target;
+// console.log(value);
 
+if (value.classList.contains('prevBtn')) {
+    counterValue--;
+    
+}
+else if (value.classList.contains('nextBtn')){
+    counterValue++;
+}
+counter.textContet = counterValue;
+console.log(counterValue);
+
+// change color
+if (counterValue === 0){
+    counter.style.color = '#333';
+}
+else if (counter < 0){
+    counter.style.color = '#F6511d';
+}
+else if (counter > 0){
+    counter.style.color = '#7fb800';
+}
+     });
+    });
 })();
