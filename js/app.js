ac05c06ac608e3a24808adfbe98932bda90c3a29
John <ema8720@gmail.com>
2019-01-04 23:28:00 -0800

    working logic

--- js/app.js
+++ js/app.js
@@ -2,37 +2,35 @@
 (function () {
 // create counter value
 let counterValue = 0;
-// get button with 
 const buttons = document.querySelectorAll('.counterBtn');
 const counter = document.getElementById('counter');
 
 
-buttons.forEach(function(btn) {
-btn.addEventListener('click', function(event){
-// e.preventDefault();
-// set up an element that we click
+buttons.forEach(function(btn){
+    btn.addEventListener('click', function(event){
+        // This is going to return the element that I am clicking
 const value = event.target;
 // console.log(value);
 
-if (value.classList.contains('prevBtn')) {
+// console.log(value);
+if (value.classList.contains('prevBtn')){
     counterValue--;
-    
 }
 else if (value.classList.contains('nextBtn')){
     counterValue++;
 }
-counter.textContet = counterValue;
-console.log(counterValue);
+counter.textContent = counterValue;
+// console.log(counterValue);
 
 // change color
-if (counterValue === 0){
-    counter.style.color = '#333';
+if ( counterValue === 0) {
+    counter.style.color = "#333";
 }
-else if (counter < 0){
-    counter.style.color = '#F6511d';
+else if (counterValue < 0) {
+    counter.style.color = "#F6511d";
 }
-else if (counter > 0){
-    counter.style.color = '#7fb800';
+else if (counterValue > 0) {
+    counter.style.color = "#7FB800";
 }
      });
     });
