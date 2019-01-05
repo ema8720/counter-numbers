39cc1f2e335afac935e6bc0395b8436928e016cf
John <ema8720@gmail.com>
2019-01-04 22:21:51 -0800

    create counnter values

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// immediate invoke function expression
+(function(){
+// create counter value
+let counterValue = 0;
+// get button with 
+const buttons = document.querySelectorAll('.counterBtn');
+const counter = document.getElementById('counter');
+
+
+
+})();
